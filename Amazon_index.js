4930f4c3274206ea3ff4c7d518a3b8f0862524eb
Cem Akdemir <cem.akd@hotmail.com>
2018-01-09 20:17:22 -0500

    Initial fruits vs vedgetable game code probably wont work

--- Amazon_index.js
+++ Amazon_index.js
@@ -2,61 +2,17 @@ exports.handler = (event, context) => {
 
   try {
     var session = event.session;
-    var suits = ["spades", "hearts", "clubs", "diamonds"];
-    var ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"];
-    var soloActions = [
-      "stand up",
-      "squat for 15 seconds or drink",
-      "sit down",
-      "do 5 push ups or drink",
-      "do 5 crunches or drink",
-      "drink with anyone you choose",
-      "drink",
-      "remain silent for three rounds",
-      "pick a person to mute for three rounds",
-      "do 5 jumping jacks or drink",
-      "drink if you have an ace",
-      "give your aces to anyone you choose",
-      "spin yourself around 5 times"
+    var plantList = [
+      {"name" : "tomato", "type" : "fruit" },
+      {"name" : "potato", "type" : "vedgetable" },
+      {"name" : "banana", "type" : "fruit" },
     ];
-    var doubleActions = [
-      "slap",
-      "drink with",
-      "swap drinks with",
-      "jump with",
-      "dab with",
-      "make a toast and drink with",
-      "dance with",
-      "give a ten second back massage to",
-      "high five with",
-      "take one random card from",
-      "take a selfie with",
-      "swap places with",
-      "swap hands with",
-      "give one of your cards to"
-    ];
-    var groupActions = [
-      "Everyone drink!",
-      "Everyone drink!",
-      "Everyone standing, drink!",
-      "Everyone sitting, drink!",
-      "Last one who does this drinks. 3, 2, 1, Jump!",
-      "Last one who does this drinks. 3, 2, 1, Clap!",
-      "Last one who does this drinks. 3, 2, 1, Squat!",
-      "Last one who does this drinks. 3, 2, 1, Touch your nose!",
-      "Last one who does this drinks. 3, 2, 1, Touch the ground!",
-      "Last one who does this drinks. 3, 2, 1, Touch your feet!",
-      "Drink for every Ace in your hand and give your Aces to someone else."
-    ];
-    var miniGameActions = [
-      "Play rock, paper, scissors versus",
-      "Play red hands versus",
-      "Play thumb wars versus",
-      "Enter a staring contest versus"
-    ];
+    var plantIndex = 0;
+    
     if (event.session.new) {
       // New Session
       console.log("NEW SESSION")
+      session.attributes.plantIndex = -1;
     }
 
     switch (event.request.type) {
@@ -66,7 +22,7 @@ exports.handler = (event, context) => {
         console.log(`LAUNCH REQUEST`);
         context.succeed(
           generateResponse(
-            buildSpeechletResponse("Party Game! Are you ready to play?", false)
+            buildSpeechletResponse("Welcome to fruits versus veggies! Ready to test your knowledge?", false)
           )
         );
         break;
@@ -74,62 +30,59 @@ exports.handler = (event, context) => {
       case "IntentRequest":
         // Intent Request
         console.log("YES INTENT REQUEST");
-
         switch(event.request.intent.name) {
           case "AMAZON.YesIntent":
-            var totalOptions = soloActions.length + doubleActions.length + groupActions.length + miniGameActions.length;
-
-            var randomNum = getRandomInt(0, totalOptions);
-            var remainingOptions = totalOptions - soloActions.length;
-            if (randomNum >= remainingOptions) { // Single
-              var actionIndex = getRandomInt(0, soloActions.length);
-              buildResponse(context, "If you have, " + 
-                getRandomCard(suits, ranks) + ", " + soloActions[actionIndex] + 
-                ". Next?", false);
+            if (session.attributes.plantIndex == -1) {
+              var totalOptions = plantList.length;
+              var plantIndex = getRandomInt(0, plantList.length);
+              session.attributes = {"plantIndex" : plantIndex};
+              buildResponse(context, "Is a " + plantList[plantIndex].name + " a fruit?", false);
             }
-            remainingOptions = remainingOptions - doubleActions.length;
-            if (randomNum >= remainingOptions) { // Double
-              var actionIndex = getRandomInt(0, doubleActions.length); 
-              buildResponse(context, "If you have, " + 
-                getRandomCard(suits, ranks) + ", " + doubleActions[actionIndex] + 
-                " the person who has " + getRandomCard(suits, ranks) + ". Next?", false);
-            }
-            remainingOptions = remainingOptions - groupActions.length;
-            if (randomNum >= remainingOptions) { // Group
-              var actionIndex = getRandomInt(0, groupActions.length); 
-              buildResponse(context, groupActions[actionIndex] + " Next?", false);
-            }
-            remainingOptions = remainingOptions - miniGameActions.length;
-            if (randomNum >= remainingOptions) { // Versus
-              var actionIndex = getRandomInt(0, miniGameActions.length);
-              buildResponse(context, "Loser drinks! The person who has " + getRandomCard(suits, ranks) + 
-                ", " + miniGameActions[actionIndex] + " the person who has " + 
-                getRandomCard(suits, ranks) + ". Next?", false);
+            else {
+              var plantIndex = session.attributes.plantIndex;
+              session.attributes.plantIndex = -1;
+              if (plantList[plantIndex].type == "fruit") {
+                buildResponse(context, "Yay!, You are right!" + plantList[plantIndex].name + " is a fruit! Do you want another question?", false);
+              }
+              else {
+                buildResponse(context, "Oh no!" + plantList[plantIndex].name + " is actually a vedgetable. Would you like another question?", false);
+              }
             }
             break;
           case "AMAZON.NoIntent":
-            var exitResponses = [
-              "Launch me again when you are ready!",
-              "Thanks for playing!",
-              "Thanks! If you enjoyed playing please rate me in the Alexa app.",
-              "See you next time!",
-              "Hope to see you again soon!",
-              "Alright, I wish you had fun!"
-            ];
-            var randomNum = getRandomInt(0, exitResponses.length);
-            buildResponse(context, exitResponses[randomNum], true);
+            if (session.attributes.plantIndex != -1) {
+              var plantIndex = session.attributes.plantIndex;
+              session.attributes.plantIndex = -1;
+              if (plantList[plantIndex].type == "fruit") {
+                buildResponse(context, "Yay!, You are right!" + plantList[plantIndex].name + " is a vedgetable! Do you want another question?", false);
+              }
+              else {
+                buildResponse(context, "Oh no!" + plantList[plantIndex].name + " is actually a fruit. Would you like another question?", false);
+              }
+            }
+            else {
+              var exitResponses = [
+                "Launch me again when you are ready!",
+                "Thanks for playing!",
+                "Thanks! If you enjoyed playing please rate me in the Alexa app.",
+                "See you next time!",
+                "Hope to see you again soon!",
+                "Alright, I wish you had fun!"
+              ];
+              var randomNum = getRandomInt(0, exitResponses.length);
+              buildResponse(context, exitResponses[randomNum], true);
+            }
             break;
           case "AMAZON.HelpIntent":
-            buildResponse(context, "Deal an entire deck of cards to all players. " +
-              "Wait for everyone to sort their hands and sit down. " + 
-              "When you are ready, say launch Party Game and follow the instructions. " + 
-              "Have fun!", false);
+            buildResponse(context, "Want to test your knowledge in classifying plants? " +
+              "I'll ask you to distinguish fruits and vedgetables from each other and " +
+              "you can test your knowledge. Are you ready to play?", false);
             break;
           case "AMAZON.CancelIntent":
-            buildResponse(context,"",true);
+            buildResponse(context, "", true);
             break;
           case "AMAZON.StopIntent":
-            buildResponse(context,"",true);
+            buildResponse(context, "", true);
             break;          
           default:
             throw "Invalid intent"
@@ -150,7 +103,6 @@ exports.handler = (event, context) => {
 
 // Helpers
 buildSpeechletResponse = (outputText, shouldEndSession) => {
-
   return {
     outputSpeech: {
       type: "PlainText",
@@ -188,20 +140,11 @@ buildResponse = (context, output, shouldEndSession) => {
   );
 }
 
-buildReprompt = (context, output) => {
+buildResponseWithSessionAttribute = (context, output, shouldEndSession, session, sessionAttributes) => {
+  
   context.succeed(
     generateResponse(
-      buildSpeechletReprompt(output, false)
+      buildSpeechletResponse(output, shouldEndSession)
     )
   );
-}
-
-function getRandomCard(suits, ranks) {
-  var randomRankIndex = getRandomInt(0, ranks.length);
-  var randomSuitIndex = getRandomInt(0, suits.length);
-  return ranks[randomRankIndex] + " of " + suits[randomSuitIndex];
-}
-
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
 }
